006cadbfb3db48c2fe32e36501496eea4cd823df
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-08 07:56:38 +0100

    Modification
    There has been some little modification in the file.

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 /*eslint-env es6*/
 const question = document.getElementById("question");
-const choice = Array.from(document.getElementsByClassName("choice-text"));
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -10,7 +10,7 @@ let availableQuestions = [];
 
 let questions = [
     {
-        question:"Inside which html element do we put the JavaScript??",
+        question: "Inside which html element do we put the JavaScript??",
         choice1:"<script>",
         choice2:"<javascript>",
         choice3:"<js>",
@@ -19,7 +19,7 @@ let questions = [
     },
     
     {
-        question:"What is the correct syntax for referring to an external script called 'xxx.js'??",
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'??",
         choice1:"<script href = 'xxx.js'>",
         choice2:"<script name'xxx.js'>",
         choice3:"<script src = 'xxx.js'>",
@@ -28,7 +28,7 @@ let questions = [
     },
     
     {
-        question:"How do you write 'Hello World' in an alert box?",
+        question: "How do you write 'Hello World' in an alert box?",
         choice1:"msgBox('Hello world' in an alert box);",
         choice2:"alertBox('Hello world');",
         choice3:"msg('Hello world');",
@@ -36,7 +36,7 @@ let questions = [
         answer: 4
     }
     
-]
+];
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -52,35 +52,32 @@ startGame = () => {
 
 getNewQuestion = () =>{
     if(availableQuestions == 0 || questionCounter >= MAX_QUESTIONS){
-        return window.location.assign('/end.html');
+        return window.location.assign("/end.html");
     }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerTest = currentQuestion.question;
-    
-}
+    question.innerText = currentQuestion.question;
+};
 choice.forEach( choice => {
-    const number = choice.data['number'];
-    choice.innerTest = currentQuestion['choice' + number];
-    
+    const number = choice.dataset["numbe"];
+    choice.innerTest = currentQuestion["choice" + number]; 
 });
 
 availableQuestions.splice(questionIndex, 1);
-console.log(availableQuestions);
 acceptingAnswers = true;
 };
 
 choice.forEach(choice => {
-    choice.addEventListener('click', e => {
+    choice.addEventListener("click", e => {
        if(!acceptingAnswers) return;
         
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+        const selectedAnswer = selectedChoice.dataset["number"];
         getNewQuestion();
     });
-})
+});
 
 
 startGame();
