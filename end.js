80fda0923011dd9ee45e56a818fa87b6777fdce9
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-09 02:35:23 +0100

    Modification
    The code to save the high score in local storage has been added.

--- end.js
+++ end.js
@@ -3,13 +3,26 @@ const username = document.getElementById("username");
 const saveScoreButton = document.getElementById(saveScoreBtn);
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
+const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
     
 username.addEventListener("keyup", () => {
     saveHighScore.disabled = !username.value;
  });
-saveHighScore = 0 =>{
+saveHighScore = 0 =={
     console.log("clicked the save button");
     e.preventDefault();
+    
+    const score = {
+        score:Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    
+    highScores.push(score);
+    highScores.sort((a,b) => b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem("highScores".JSON.stringify(highScores));
+window.location.assign(*/*);
 }
 
