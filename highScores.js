4dc0beca9693f351a94e243527f8a577c8e57bed
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-09 03:37:44 +0100

    New html, css and js
    created new high scores HTML page, CSS, and js file

---
+++ highScores.js
@@ -0,0 +1,9 @@
+/*eslint-env es6*/
+const highScoresList = document.getElementById("highScoresList");
+const highScore = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores.map(score == {
+    return '<li class="high-score">${score.name} - ${score.score}</li>';
+    .addEventListener(''.(0) =>{})
+});
+.join("");
