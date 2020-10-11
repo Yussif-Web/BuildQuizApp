2615522679e59275f697208a45d8f83fb25fa52a
Yussif-Web <72410610+Yussif-Web@users.noreply.github.com>
2020-10-11 08:45:39 +0100

    Code modification

--- game.js
+++ game.js
@@ -11,34 +11,18 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which html element do we put the JavaScript??",
-        choice1:"<script>",
-        choice2:"<javascript>",
-        choice3:"<js>",
-        choice4:"<scripting>",
-        answer: 1
-    },
-    
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'??",
-        choice1:"<script href = 'xxx.js'>",
-        choice2:"<script name'xxx.js'>",
-        choice3:"<script src = 'xxx.js'>",
-        choice4:"<script file = 'xxx.js'>",
-        answer: 3
-    },
-    
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        choice1:"msgBox('Hello world' in an alert box);",
-        choice2:"alertBox('Hello world');",
-        choice3:"msg('Hello world');",
-        choice4:"alert('Hello world')",
-        answer: 4
-    }
-    ];
+let questions = [];
+
+fetch("questions.json").then(res=>{
+    return res.json();
+}).then(loadedQuestions => {
+    questions = loadedQuestions;
+    startGame ();
+})
+.catch(err =>{
+    console.error(err);
+});
+
 
 //CONSTANTS
 const CORRECT_BONUS =10;
@@ -52,12 +36,13 @@ startGame = () => {
 };
 
 getNewQuestion = () =>{
-    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        localStorage.setItem("mostRecentScore", score);
         return window.location.assign("/end.html");
     }
     questionCounter++;
     progressText.innerText = Question ${questionCounter}/${MAX_QUESTIONS};
-    progressBarFull.style.width = '(${questionCounter/MAX_QUESTIONS) * 100}px';  
+    progressBarFull.style.width = '(${questionCounter/MAX_QUESTIONS) * 100}%';  
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -76,13 +61,13 @@ choice.forEach(choice => {
         
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number";
+        const selectedAnswer = selectedChoice.dataset["number"];
         
-        const classToApply = 'incorrect';
-        if (selectedAnswer == currentQuestion.answer){
-            classToApply = 'correct';
+        const classToApply = 
+        selectedAnswer == currentQuestion.answer?
+            "correct" : "incorrect";
             
-           if(classToApply === 'correct') {
+           if(classToApply == "correct") {
                incrementScore(CORRECT_BONUS);
            }
             selectedChoice.parentElement.classList.add(classToApply);
@@ -91,6 +76,8 @@ choice.forEach(choice => {
         }
         
         getNewQuestion();
+        }; 1000);
+
     });
 });
 
@@ -99,7 +86,6 @@ incrementScore = num => {
     scoreText.innerText = score;
 };
 
-startGame();
 
 
 
